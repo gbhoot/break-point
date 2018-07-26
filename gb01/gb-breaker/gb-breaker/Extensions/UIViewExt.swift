4da4db9bba6411e516380e5837808719241ce4bd
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-26 13:37:26 -0700

    fixed the button frame change

--- gb01/gb-breaker/gb-breaker/Extensions/UIViewExt.swift
+++ gb01/gb-breaker/gb-breaker/Extensions/UIViewExt.swift
@@ -21,9 +21,9 @@ extension UIView {
     @objc func keyboardFrameChange(_ notification: NSNotification) {
         
         let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
-        let startingY = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue.origin.y
-        let endingY = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.origin.y
-        let deltaY = endingY - startingY
+        let startingY = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue//.origin.y
+        let endingY = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue//.origin.y
+        let deltaY = endingY.origin.y - startingY.origin.y
         
         UIView.animate(withDuration: duration) {
             self.frame.origin.y += deltaY
